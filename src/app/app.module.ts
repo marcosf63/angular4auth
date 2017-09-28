3fb918d942e4193da7c283f5ef15b7c994bcff3c
Marcos Oliveira <marcosf63@gmail.com>
2017-09-28 17:21:52 -0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthService } from './services/auth.service';
+import { RegisterComponent } from './components/register/register.component';
+import { StatusComponent } from './components/status/status.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    FormsModule,
     RouterModule.forRoot([
-      { path: 'login', component: LoginComponent }
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'status', component: StatusComponent }
     ])
   ],
   providers: [AuthService],
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,32 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
 })
-export class LoginComponent implements OnInit {
+export class RegisterComponent implements OnInit {
 
   user: User = new User();
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
-
   }
 
-  onLogin(): void {
-    this.auth.login(this.user)
+  onRegister(): void {
+    this.auth.register(this.user)
     .then((user) => {
       localStorage.setItem('token', user.json().auth_token);
-      this.router.navigateByUrl('/status');
     })
     .catch((err) => {
       console.log(err);
     });
   }
-
 }
